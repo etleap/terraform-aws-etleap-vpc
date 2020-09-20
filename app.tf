3736e9f3ff7b2ad5bd676eeeeb5b2ddbbc0aa0ca
Christian Romming <christian@etleap.com>
2020-09-20 18:41:39 +0200

    Fix.

--- app.tf
+++ app.tf
@@ -1,17 +1,20 @@
 resource "aws_instance" "app" {
   depends_on = [
-    aws_route53_record.db,
     aws_route.prod_public,
     aws_emr_cluster.emr
   ]
 
   instance_type               = "t3.large"
   ami                         = var.amis["app"]
-  subnet_id                   = aws_subnet.b_public.id
   key_name                    = var.key_name
-  vpc_security_group_ids      = [aws_security_group.app.id]
   associate_public_ip_address = true
   iam_instance_profile        = aws_iam_instance_profile.app.name
+
+  network_interface {
+    network_interface_id = aws_network_interface.app.id
+    device_index         = 0
+  }
+
   user_data                   = <<EOF
 #cloud-config
 # -*- YAML -*-
@@ -115,6 +118,11 @@ resource "aws_iam_role" "app" {
 }
 EOF
 
+}
+
+resource "aws_network_interface" "app" {
+  subnet_id       = aws_subnet.b_public.id
+  security_groups = [aws_security_group.app.id]
 }
 
 output "app-ip-address" {
