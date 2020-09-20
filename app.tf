26d620bff42c7e4b3064db04b265f2e64b1961db
Christian Romming <christian@etleap.com>
2020-09-20 21:06:45 +0200

    Fix.

--- app.tf
+++ app.tf
@@ -8,7 +8,12 @@ resource "aws_instance" "app" {
   ami                         = var.amis["app"]
   key_name                    = var.key_name
   iam_instance_profile        = aws_iam_instance_profile.app.name
-  subnet_id                   = aws_subnet.b_public.id
+
+  network_interface {
+    network_interface_id = aws_network_interface.app.id
+    device_index         = 0
+  }
+
   user_data                   = <<EOF
 #cloud-config
 # -*- YAML -*-
@@ -83,10 +88,6 @@ EOF
   volume_tags = {
     Name = "Etleap App"
   }
-
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
 resource "aws_iam_instance_profile" "app" {
@@ -117,12 +118,6 @@ EOF
 resource "aws_network_interface" "app" {
   subnet_id       = aws_subnet.b_public.id
   security_groups = [aws_security_group.app.id]
-}
-
-resource "aws_network_interface_attachment" "app" {
-  instance_id          = aws_instance.app.id
-  network_interface_id = aws_network_interface.app.id
-  device_index         = 1
 }
 
 resource "aws_eip" "app" {
