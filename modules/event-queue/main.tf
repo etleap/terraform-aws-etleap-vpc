66d130f506c5d55457f0a02d5ddcc5f5f29ee211
ZygimantasKoncius <zygimantas@etleap.com>
2021-09-01 16:39:37 -0700

    introduced dlq and enabled raw message delivery

--- modules/event-queue/main.tf
+++ modules/event-queue/main.tf
@@ -9,10 +9,18 @@ resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
   topic_arn = aws_sns_topic.etleap-inbound.arn
   protocol  = "sqs"
   endpoint  = aws_sqs_queue.etleap-inbound.arn
+  raw_message_delivery = true
 }
 
+resource "aws_sqs_queue" "etleap-inbound-dlq" {
+  name = "etleap-inbound-dlq-${var.organization}"
+}
 resource "aws_sqs_queue" "etleap-inbound" {
   name = "etleap-inbound-${var.organization}"
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.etleap-inbound-dlq.arn
+    maxReceiveCount     = 3
+  })
 }
 
 resource "aws_sqs_queue_policy" "allow-sns-write" {
@@ -32,6 +40,30 @@ resource "aws_sqs_queue_policy" "allow-sns-write" {
     "Condition": {
       "ArnLike": {
         "aws:SourceArn": "${aws_sns_topic.etleap-inbound.arn}"
+      }
+    }
+  }]
+}
+EOF
+}
+
+resource "aws_sqs_queue_policy" "allow-sqs-write-dlq" {
+  queue_url = aws_sqs_queue.etleap-inbound-dlq.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Id": "${aws_sqs_queue.etleap-inbound-dlq.arn}/SQSDLQDefaultPolicy",
+  "Statement": [{
+    "Sid": "main-sqs-arn:${aws_sqs_queue.etleap-inbound.arn}",
+    "Effect": "Allow",
+    "Principal": {
+      "AWS": "*"
+    },
+    "Action": "SQS:SendMessage",
+    "Resource": "${aws_sqs_queue.etleap-inbound-dlq.arn}",
+    "Condition": {
+      "ArnLike": {
+        "aws:SourceArn": "${aws_sqs_queue.etleap-inbound.arn}"
       }
     }
   }]
