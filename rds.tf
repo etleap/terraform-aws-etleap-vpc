9c5b358ebef6a9d0639b13d792865b89c57575a5
Caius Brindescu <caius@etleap.com>
2022-02-18 16:31:21 -0800

    Updated to MySQL 5.7

--- rds.tf
+++ rds.tf
@@ -3,13 +3,13 @@ resource "aws_db_instance" "db" {
   allocated_storage            = 500
   storage_type                 = "gp2"
   engine                       = "mysql"
-  engine_version               = "5.6.41"
+  engine_version               = "5.7.34"
   instance_class               = "db.m5.large"
   name                         = "EtleapDB"
   username                     = "root"
   password                     = module.db_root_password.secret_string
   db_subnet_group_name         = aws_db_subnet_group.db.name
-  parameter_group_name         = aws_db_parameter_group.mysql5-6-etleap.name
+  parameter_group_name         = aws_db_parameter_group.mysql5-7-etleap.name
   vpc_security_group_ids       = [aws_security_group.db.id]
   backup_retention_period      = 7
   auto_minor_version_upgrade   = false
@@ -26,6 +26,9 @@ resource "aws_db_instance" "db" {
       name
     ]
   }
+
+  allow_major_version_upgrade = true
+  apply_immediately           = true
 }
 
 resource "aws_db_subnet_group" "db" {
@@ -44,6 +47,102 @@ resource "aws_db_parameter_group" "mysql5-6-etleap" {
   name        = "etleap-mysql5-6-${random_id.deployment_random.hex}"
   description = "MySQL 5.6 with Etleap modifications"
   family      = "mysql5.6"
+
+  parameter {
+    name         = "max_allowed_packet"
+    value        = 546308096
+    apply_method = "pending-reboot"
+  }
+
+  parameter {
+    name         = "character-set-client-handshake"
+    value        = 0
+    apply_method = "pending-reboot"
+  }
+
+  parameter {
+    name         = "collation_server"
+    value        = "latin1_swedish_ci"
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "character_set_server"
+    value        = "latin1"
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "log_bin_trust_function_creators"
+    value        = 1
+    apply_method = "pending-reboot"
+  }
+
+  parameter {
+    name         = "binlog_format"
+    value        = "ROW"
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "innodb_lock_wait_timeout"
+    value        = 250
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "general_log"
+    value        = "0"
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "long_query_time"
+    value        = 0.05
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "slow_query_log"
+    value        = 1
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "log_warnings"
+    value        = 2
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "slow_query_log"
+    value        = 1
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "log_output"
+    value        = "file"
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "binlog_checksum"
+    value        = "none"
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "innodb_log_file_size"
+    value        = 5463080960 # 10x max_allowed_packet
+    apply_method = "pending-reboot"
+  }
+}
+
+resource "aws_db_parameter_group" "mysql5-7-etleap" {
+  name        = "etleap-mysql5-7-${random_id.deployment_random.hex}"
+  description = "MySQL 5.7 with Etleap modifications"
+  family      = "mysql5.7"
 
   parameter {
     name         = "max_allowed_packet"
