be4d46db7f52e5cea9b473a3550a666b38caf7d6
Caius Brindescu <caius@etleap.com>
2023-10-04 12:36:12 -0700

    Allow TLS1.2 for Streaming Ingestion Endpoints
    This allows both TLS 1.3 and TLS 1.2 for Streaming Ingestion Endpoints.

--- modules/elva/lb.tf
+++ modules/elva/lb.tf
@@ -45,7 +45,7 @@ resource "aws_lb_listener" "elva_https" {
   load_balancer_arn = aws_lb.elva.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-TLS13-1-3-2021-06"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   certificate_arn   = var.acm_certificate_arn
 
   default_action {
