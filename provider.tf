9fb50c56c4a09bfff83f6c61039fb2b560c440f4
shreyaramakrishnan <81744561+shreyaramakrishnan@users.noreply.github.com>
2024-08-21 23:33:46 -0700

    Upgrade AWS Provider Version (#153)

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.28"
+      version = "~> 5.61"
     }
     null = {
       source = "hashicorp/null"
